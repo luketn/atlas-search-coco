390c6486bd9cdca2d6148ac277f15a8bed86ecbc
luketn <endeavour9@gmail.com>
2024-12-10 11:30:49 +1100

    cleanup and reorder

--- src/main/java/com/mycodefu/model/ImageSearchResult.java
+++ src/main/java/com/mycodefu/model/ImageSearchResult.java
@@ -1,13 +1,10 @@
 package com.mycodefu.model;
 
-import org.bson.Document;
-
 import java.util.List;
 
 public record ImageSearchResult(List<Image> docs, List<ImageMeta> meta) {
+    public record ImageMeta (ImageMetaTotal count, ImageMetaFacets facet) { }
     public record ImageMetaTotal (long total) { }
-    public record ImageMetaFacetBucket (String _id, long count) { }
-    public record ImageMetaFacet (List<ImageMetaFacetBucket> buckets) { }
     public record ImageMetaFacets (
             ImageMetaFacet accessory,
             ImageMetaFacet animal,
@@ -21,5 +18,6 @@ public record ImageSearchResult(List<Image> docs, List<ImageMeta> meta) {
             ImageMetaFacet sports,
             ImageMetaFacet vehicle
     ) { }
-    public record ImageMeta (ImageMetaTotal count, ImageMetaFacets facet) { }
+    public record ImageMetaFacet (List<ImageMetaFacetBucket> buckets) { }
+    public record ImageMetaFacetBucket (String _id, long count) { }
 }
